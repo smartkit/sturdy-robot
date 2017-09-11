792439f2927fd8560baa52aba6825e5a3a16ba60
Knight.Zhou <YoungWelle@gmail.com>
2017-09-11 13:29:14 +0800

    CHANGED:grayscale trainning images,added testing images;

--- saas/CaffeSegNetCMR4DataDreams/SegNetCMR/inputs.py
+++ saas/CaffeSegNetCMR4DataDreams/SegNetCMR/inputs.py
@@ -2,8 +2,8 @@ import tensorflow as tf
 
 def placeholder_inputs(batch_size):
 
-    images = tf.placeholder(tf.float32, [batch_size, 2048, 2048, 1])
-    labels = tf.placeholder(tf.int64, [batch_size, 2048, 2048])
+    images = tf.placeholder(tf.float32, [batch_size, 256, 256, 1])
+    labels = tf.placeholder(tf.int64, [batch_size, 256, 256])
     is_training = tf.placeholder(tf.bool)
 
     return images, labels, is_training
