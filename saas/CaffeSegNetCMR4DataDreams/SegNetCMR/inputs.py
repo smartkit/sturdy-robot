df242b719aae0f9a6f1cb063f1140a8d28732a00
Knight.Zhou <YoungWelle@gmail.com>
2017-09-18 23:15:35 +0800

    CHANGED:re-organized the test/train images;

--- saas/CaffeSegNetCMR4DataDreams/SegNetCMR/inputs.py
+++ saas/CaffeSegNetCMR4DataDreams/SegNetCMR/inputs.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 
 def placeholder_inputs(batch_size):
 
-    images = tf.placeholder(tf.float32, [batch_size, 256, 256, 1])
+    images = tf.placeholder(tf.float32, [batch_size, 256, 256,1])
     labels = tf.placeholder(tf.int64, [batch_size, 256, 256])
     is_training = tf.placeholder(tf.bool)
 
